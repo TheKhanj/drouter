fc6f80a81c4fff13c7cfff3e5b7574b826ccd457
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-09-12 11:50:32 +0330

    implement basic features of router
    I've omitted porting some features of httprouter for now just for the
    sake of simplicity, but I will comeback to it in final refactor phase.
    Right now I just want something working. It doesn't have to be perfect.

--- router.go
+++ router.go
@@ -1,5 +1,9 @@
 package drouter
 
+import "sync"
+
+type tsr bool
+
 type Param struct {
 	Key   string
 	Value string
@@ -15,12 +19,81 @@ type Handler interface {
 	Handle(params Params)
 }
 
-type Router struct{}
+type Router struct {
+	root *node
 
-type Tsr bool
+	paramsPool sync.Pool
+	maxParams  uint16
+}
 
-func (r *Router) Lookup(path string) (Handler, Tsr) {
+func (r *Router) getParams() *Params {
+	ps, _ := r.paramsPool.Get().(*Params)
+	*ps = (*ps)[0:0]
+	return ps
+}
+
+func (r *Router) putParams(ps *Params) {
+	if ps != nil {
+		r.paramsPool.Put(ps)
+	}
+}
+
+func (r *Router) Lookup(path string) (Handler, Params, tsr) {
+	root := r.root
+
+	if root == nil {
+		return nil, nil, false
+	}
+
+	handler, ps, tsr := root.getValue(path, r.getParams)
+
+	if handler == nil {
+		r.putParams(ps)
+		return nil, nil, tsr
+	}
+
+	if ps == nil {
+		return handler, nil, tsr
+	}
+
+	return handler, *ps, tsr
 }
 
 func (r *Router) AddRoute(path string, handler Handler) {
+	if len(path) < 1 || path[0] != '/' {
+		panic("path must begin with '/' in path '" + path + "'")
+	}
+
+	if handler == nil {
+		panic("handler must not be nil")
+	}
+
+	root := r.root
+
+	if root == nil {
+		root = new(node)
+		r.root = root
+	}
+
+	root.addRoute(path, handler)
+
+	r.updateMaxParams(path)
+	r.lazyInitParamsPool()
+}
+
+func (r *Router) lazyInitParamsPool() {
+	if !(r.paramsPool.New == nil && r.maxParams > 0) {
+		return
+	}
+
+	r.paramsPool.New = func() interface{} {
+		ps := make(Params, 0, r.maxParams)
+		return &ps
+	}
+}
+
+func (r *Router) updateMaxParams(path string) {
+	if paramsCount := countParams(path); paramsCount > r.maxParams {
+		r.maxParams = paramsCount
+	}
 }
