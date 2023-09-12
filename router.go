c42920ee28364877c5f98b9b58bdd6d40a94acad
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-09-12 11:20:43 +0330

    another commit, router stuff
    for real usually I don't write shitty commit messages like this. OK, I'm
    not gonna do it anymore in this project.

--- router.go
+++ router.go
@@ -1,7 +1,5 @@
 package drouter
 
-type Router struct{}
-
 type Param struct {
 	Key   string
 	Value string
@@ -15,4 +13,14 @@ var ParamsKey = paramsKey{}
 
 type Handler interface {
 	Handle(params Params)
+}
+
+type Router struct{}
+
+type Tsr bool
+
+func (r *Router) Lookup(path string) (Handler, Tsr) {
+}
+
+func (r *Router) AddRoute(path string, handler Handler) {
 }
